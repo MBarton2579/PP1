16ebfbee394cab3f2572d0db06212d8230207c4e
MBarton2579 <mrbarton@ualr.edu>
2021-05-26 20:59:17 -0500

    1

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,32 @@
 #include <string>
+#include <random>
+#include <vector>
+
+struct Object {
+	std::string name;
+	int strength{ 0 };
+	int health{ 0 };
+
+};
+
+int main() {
+
+	Object Player{ "Billy Wild", 0, 0 };
+
+	std::random_device seed;
+	std::default_random_engine engine(seed());
+	std::normal_distribution<double> strength(30.0, 5.0);
+	std::normal_distribution<double> health(5.0, 1.0);
+
+	Player.strength = std::max(1, (int)strength(engine));
+	Player.health = std::max(1, (int)health(engine));
+
+	std::vector<Object> Monsters;
+
+	std::uniform_int_distribution<double> createMonsters(1.0, 3.0);
+
+
+
+
+
+}
